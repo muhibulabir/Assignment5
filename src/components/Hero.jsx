631ee4293e8e81956c765cf624c9462fcf49656d
muhibulabir <muhibulabir@gmail.com>
2026-09-13 17:49:34 +0600

    hero section file

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import heroImage from "../assets/banner-stack.png";
+function Hero() {
+    return (
+        <section id="home" className="mx-auto max-w-7xl px-4 pb-16 pt-14 sm:px-6 lg:px-8 lg:pb-24 lg:pt-20">
+            <div className="grid items-center gap-12 lg:grid-cols-2">
+                <div>
+                    <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">
+                        Build your Ideal<span className="text-gradient-brand">Devolopment Stack</span>
+                    </h1>
+                    
+                </div>
+
+            </div>
+            </section>
+    )
+}
