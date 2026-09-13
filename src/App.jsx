d75ac2cee76f3e4ca12d39dc272f73808d1e5155
muhibulabir <muhibulabir@gmail.com>
2026-09-13 17:22:17 +0600

    cinfig file added also index css file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,59 @@
-import { useState } from 'react'
-import {ToastContainer, toast} from 'react-toastify'
+import { useEffect, useMemo, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
 import Navbar from './components/Navbar'
 
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [technologies, setTechnologies] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [stack, setStack] = useState([])
 
- return (
+  useEffect(() => {
+    let isMounted = true
+
+    fetch('/technologies.json')
+      .then((res) => res.json())
+      .then((data) => {
+        if (isMounted) setTechnologies(data)
+      })
+      .catch(() => {
+        if (isMounted) toast.error('Could not load technology data.')
+      })
+      .finally(() => {
+        if (isMounted) setLoading(false)
+      })
+
+    return () => {
+      isMounted = false
+    }
+  }, [])
+
+  const stackIds = useMemo(() => new Set(stack.map((t) => t.id)), [stack])
+
+  function handleAdd(tech) {
+    if (stackIds.has(tech.id)) {
+      toast.warn(`${tech.name} is already in your stack.`)
+      return
+    }
+    setStack((prev) => [...prev, tech])
+    toast.success(`${tech.name} added to your stack.`)
+  }
+
+  function handleRemove(id) {
+    const tech = stack.find((t) => t.id === id)
+    setStack((prev) => prev.filter((t) => t.id !== id))
+    if (tech) toast.info(`${tech.name} removed from your stack.`)
+  }
+
+  function handleRemoveAll() {
+    if (stack.length === 0) return
+    setStack([])
+    toast.info('Your stack has been cleared.')
+  }
+
+  return (
     <div id="top" className="min-h-screen bg-white">
       <Navbar />
       <Hero />
@@ -29,12 +75,7 @@ function App() {
       </section>
 
       <Footer />
-
-      <ToastContainer position="top-right" autoClose={2500} newestOnTop />
+      <ToastContainer position="bottom-right" autoClose={2500} newestOnTop />
     </div>
   )
 }
-  
-  
-
-export default App
